5667f646219d69005e5d8b5c17af2858615ddf19
MWMS-LLC <info@myworldmysay.com>
2026-01-03 08:58:24 -0500

    Configure parents_chinese1230 for parents-chinese-poll deployment - Update configs for Chinese version, add constants file, update URLs

---
+++ frontend/src/constants/components.zh.js
@@ -0,0 +1,15 @@
+// Chinese translations for components
+export const COMPONENTS = {
+  hamburgerMenu: {
+    openMenu: '打开菜单',
+    beforeYouBegin: '开始之前',
+    pickATopic: '选择一个主题',
+    soundtrack: '配乐',
+    aboutFAQ: '关于/常见问题',
+    helpResources: '帮助资源',
+    themeSong: '主题曲',
+    dataCleared: '数据已清除！页面将刷新以重新开始。',
+    errorClearingData: '清除数据时出错。请手动刷新页面。'
+  }
+}
+
